46eee9f6b1478caa8209864b5545d602cd695f92
KJVJ <kj_vj@hotmail.com>
2020-04-12 00:52:27 +0200

    Persisting unity-unit-movement project.

---
+++ Assets/RTS_Camera/Scripts/CapsuleMovement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+// Use physics raycast hit from mouse click to set agent destination
+[RequireComponent(typeof(NavMeshAgent))]
+public class CapsuleMovement : MonoBehaviour
+{
+    RaycastHit m_HitInfo = new RaycastHit();
+    Vector3 AgentPosition;
+    NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !Input.GetKey(KeyCode.LeftShift))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
+            {
+                GetComponent<NavMeshAgent>().destination = m_HitInfo.point;
+            }
+                
+
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        SetAgentPosition();
+        NavMeshAgent m_Agent = GetComponent<NavMeshAgent>();
+        if (m_Agent != null && m_Agent.hasPath)
+        {
+            NavMeshPath m_Path = m_Agent.path;
+            Gizmos.color = Color.blue;
+            if (m_Path != null && m_Path.corners != null && m_Path.corners.Length > 0)
+            {
+                var prev = AgentPosition;
+                for (int i = 0;
+                    i < m_Path.corners.Length; ++i)
+                {
+                    Gizmos.DrawLine(prev, m_Path.corners[i]);
+                    prev = m_Path.corners[i];
+                }
+            }
+        }
+    }
+    void SetAgentPosition()
+    {
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(transform.position,
+                                  out hit, 1.0f,
+                                  NavMesh.AllAreas))
+        {
+            AgentPosition = hit.position;
+        }
+    }
+}
---
+++ Assets/RegimentFormation.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegimentFormation : MonoBehaviour
+{
+
+
+    public int rows = 4;
+    public int cols = 6;
+    public float colSpacing = 1f;
+    public float rowSpacing = 1f;
+    public float markerDiameter = 0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+        GameObject leader = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        leader.name = "Leader";
+        leader.transform.parent = this.transform;
+        leader.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        leader.transform.localPosition = new Vector3(0, 0, 0);
+        
+        Debug.Log(this.name);
+
+        
+        int sphereDiameter = 1;
+        int rSpaces = rows - 1;
+        int cSpaces = cols - 1;
+
+        //add formation markers.
+        for(int r = 0; r < rows; r++)
+        {
+            for(int c = 0; c < cols; c++)
+            {
+                // |0 <> 1 <> 2 <> 3 <> 4 <> 5|
+                // |1 <> X <> X <> X <> X <> X|
+                // |2 <> X <> X <> X <> X <> X|
+                // |3 <> X <> X <> X <> X <> X|
+                // |4 <> X <> X <> X <> X <> X|
+             
+                
+
+                GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                sphere.transform.localScale = new Vector3(markerDiameter, markerDiameter, markerDiameter);
+                sphere.name = "Formation_" + c + "_" + r;
+
+                //calculate the left corner
+                float sphereX =  (float) cols/2 -0.5f * sphereDiameter + ((float)cSpaces/2 * (float)colSpacing);
+                sphereX *= -1;
+                //add the current spheres location.
+                sphereX += (c * (sphereDiameter + colSpacing));
+
+                // y coordinate
+                float sphereZ = r * sphereDiameter + r * rowSpacing;
+                sphereZ *= -1;
+
+
+                Vector3 position = new Vector3(sphereX, 0, sphereZ);
+                Debug.Log(position);
+
+                sphere.transform.parent = this.transform;
+                sphere.transform.localPosition = position;
+            
+                
+               
+
+
+
+
+            }
+        }
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
